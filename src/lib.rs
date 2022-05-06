2368f2211af0dd8c2fc9420effff7e209bdbe35f
Alex Black <wa7@sa34.cx>
2022-05-06 22:39:37 +0300

    Logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-pub async fn run() {
-    println!("Hello, Earth!");
+//! Library module
+
+use logging::Logging;
+
+pub mod logging;
+
+pub async fn run() -> anyhow::Result<()> {
+    // Load environment variables from .env
+    dotenv::dotenv().ok();
+
+    // Initialize logging
+    Logging::from_env().init()?;
+
+    log::info!("Hello, Earth!");
+
+    Ok(())
 }
